d16423b8039d6646ac9507c2790b72b7f8eb10dc
rjunaid1807 <114145484+rjunaid1807@users.noreply.github.com>
2022-11-29 21:58:02 +0530

    Updated

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,15 +5,13 @@ function Alert(props) {
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
-  
-    return ( 
-    props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-        {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>   @@@coz it wa not working properly */}
-    </div>
-    
-  )
+    return (
+        <div style={{ height: '50px' }}>
+            {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+            </div>}
+        </div>
+    )
 }
 
 export default Alert
-
