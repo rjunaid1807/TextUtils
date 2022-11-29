f0f419090167d97c6dcd32b4e0a5360ec66887f7
rjunaid1807 <114145484+rjunaid1807@users.noreply.github.com>
2022-11-29 21:55:23 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,59 +1,55 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, {useState} from 'react';
+import About from './components/About';
+import React, { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 
 function App() {
-  const [mode, setMode] = useState('light'); //whether mode is light state or not
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
-  
 
-  const showAlert = (message, type)=> {
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
-    }) 
+    })
     setTimeout(() => {
       setAlert(null);
-    }, 3000);
+    }, 1500);
   }
 
-  // const toggleMode = ()=> {
-  //   if (mode ==='dark'){
-  //     setMode('light')
-  //     document.body.style.backgroundColor = 'white';
-  //   } else{
-  //     setMode('dark')
-  //     document.body.style.backgroundColor = 'rgb(2 30 52)';
-  //   }
-  // }
-  const lightMode = ()=> {
-      setMode('light')
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode('light');
       document.body.style.backgroundColor = 'white';
-    } 
-  
-  const blueMode = ()=> {
-    setMode('dark')
-    document.body.style.backgroundColor = 'rgb(2 30 52)';
+      showAlert("Light mode has been enabled", "success");
+    }
   }
-
-  const greenMode = ()=> {
-    setMode('dark')
-    document.body.style.backgroundColor = '#071407';
-  } 
-
-
   return (
-    
     <>
-      <Navbar title = "TextUtils" aboutText = "About Us" mode = {mode} lightMode = {lightMode} blueMode = {blueMode} greenMode = {greenMode}/>
-      <Alert alert = {alert}/>
-      <TextForm showAlert = {showAlert} heading = "Text goes here" mode = {mode}  />
-      
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+          <Route exact path='/about' element= {<About mode={mode}/>}/>
+          <Route exact path='/' element= {<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />}/>
+          </Routes>
+        </div>
+      </Router>
     </>
-  
   );
 }
 
